78dbd8f4b346366dea38a6819fc0491e4656c40b
dya-only <dyacode@proton.me>
2023-11-23 15:36:52 +0900

    feat: floor & barrier

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,57 +2,92 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-class GameWindow extends JFrame {
-    Toolkit imageTool = Toolkit.getDefaultToolkit();
-    Image dinosaur = imageTool.getImage("assets/dinosaur.png");
-    Image buffImg;
-    Graphics buffG;
+class DinoGame extends JPanel implements KeyListener, Runnable {
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int yPos = -50;
+    int jumpVelocity = 13;
+    boolean isJumping = false;
+    int barrierX = 0;
+    int barrierSpeed = 5;
 
-    double yPos = 300;
+    Image dinoImage;
+    Image floorImage;
+    Image barrier1Image;
 
-    public GameWindow() {
-        setTitle("Dinosaur Game");
-        setSize(854, 480);
-        setResizable(false);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public DinoGame() {
+        this.setFocusable(true);
+        this.addKeyListener(this);
 
-        addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    yPos -= 50;
-                }
-            }
-        });
+        dinoImage = new ImageIcon("assets/dinosaur.png").getImage();
+        floorImage = new ImageIcon("assets/floor.png").getImage();
+        barrier1Image = new ImageIcon("assets/barrier1.png").getImage();
 
+        new Thread(this).start();
     }
 
     @Override
     public void paint(Graphics g) {
-        buffImg = createImage(getWidth(), getHeight());
-        buffG = buffImg.getGraphics();
-        update(g);
+        super.paint(g);
+
+        // Player (Dinosaur)
+        g.drawImage(dinoImage, 100, 300 - yPos, 40, 40, this);
+
+        // Floor
+        g.drawImage(floorImage, barrierX, 385, 800, 6, this);
+        g.drawImage(floorImage, barrierX + 800, 385, 800, 6, this);
+
+        // Barriers
+        g.drawImage(barrier1Image, barrierX + 850, 340, 30, 50, this);
     }
 
     @Override
-    public void update(Graphics g) {
-        buffG.clearRect(0, 0, 20, 20);
-        buffG.drawImage(dinosaur, 100, (int) yPos, 50, 50, this);
-        g.drawImage(buffImg, 0, 0, this);
-        yPos += 0.2;
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        if (yPos >= 300) yPos -= 0.2;
+            if (isJumping) {
+                yPos += jumpVelocity;
+                jumpVelocity -= 1;
+                if (yPos <= -50) {
+                    yPos = -50;
+                    isJumping = false;
+                    jumpVelocity = 13;
+                }
+            }
 
-        repaint();
+            barrierX -= barrierSpeed;
+            if (barrierX < -(screenSize.getWidth() / 2 - 100)) barrierX = 0;
+
+            this.repaint();
+        }
     }
-}
 
-public class Main {
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && !isJumping) {
+            isJumping = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
     public static void main(String[] args) {
-        new GameWindow();
+        JFrame frame = new JFrame();
+        frame.setSize(800, 500);
+        frame.add(new DinoGame());
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
